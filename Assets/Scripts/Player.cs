21ab1354b70cea95bbc8542b2ac3eb7aafd78504
MY name here is <asd>
2021-03-05 13:51:48 +0100

    starting with life bonus

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,17 +4,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
-
     [SerializeField] private Joystick joystick;
     [SerializeField] private Transform groundCheckTransform;
     [SerializeField] private LayerMask playerMask;
-    
+    [SerializeField] private LayerMask bonusLifeMask;
+
     private Rigidbody rigidBodyComponent;
 
     private float runSpeed = 5f;
     private float horizontalMove = 0f;
 
     private bool isJumping = false;
+    private float jumpPower = 4f;
+
+    private int lifeCount = 2;
 
     // Start is called before the first frame update
     void Start() {
@@ -23,31 +26,29 @@ public class Player : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        
         horizontalMove = joystick.Horizontal * runSpeed;
-        
+
         rigidBodyComponent.velocity = new Vector3(horizontalMove, rigidBodyComponent.velocity.y, 0);
 
-        float verticalMove = joystick.Vertical;
-
-        if (verticalMove >= .5f) {
+        //check if joysticck up is more than set value
+        if (joystick.Vertical >= .5f) {
             isJumping = true;
         }
-
-        
-
     }
 
     private void FixedUpdate() {
+        // check if player is on ground if true return and not jump
+        if (Physics.OverlapSphere(groundCheckTransform.position, .1f, playerMask).Length == 0) return;
 
-        if (Physics.OverlapSphere(groundCheckTransform.position, .1f, playerMask).Length == 0) {
-            return;
-        }
-        
         if (isJumping) {
-            float jumpPower = 5f;
             rigidBodyComponent.AddForce(Vector3.up * jumpPower, ForceMode.VelocityChange);
             isJumping = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.layer == 9) {
+            Destroy(other.gameObject);
         }
     }
 }
