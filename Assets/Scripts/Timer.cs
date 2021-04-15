44aaa9ea9e5d9e66a9b6daaa51d7e8053674e087
MY name here is <asd>
2021-04-15 17:27:52 +0200

    added basic timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    [SerializeField] private Text text;
+
+    private void Start() {
+        Debug.Log(PlayerPrefs.GetString("timeFromStart"));
+        text.text = PlayerPrefs.GetString("timeFromStart");
+        PlayerPrefs.DeleteKey("timeFromStart");
+    }
+}
