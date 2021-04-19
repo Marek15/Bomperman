1f55e19fdb12d5fe3ce4e6f7d1ba3341bc7667ae
FilipGres <filipgress@gmail.com>
2021-04-19 07:11:00 +0200

    added characters

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform player;
+    public BoxCollider2D mapBounds;
+
+    private float xMin, xMax, yMin, yMax;
+    private float camY,camX;
+    private float camOrthsize;
+    private float cameraRatio;
+    private Camera mainCam;
+
+    private void Start(){
+        xMin = mapBounds.bounds.min.x;
+        xMax = mapBounds.bounds.max.x;
+        yMin = mapBounds.bounds.min.y;
+        yMax = mapBounds.bounds.max.y;
+        mainCam = GetComponent<Camera>();
+        camOrthsize = mainCam.orthographicSize;
+        cameraRatio = (xMax + camOrthsize) / 2.0f;
+    }
+    
+    // Update is called once per frame
+    void FixedUpdate(){
+        camY = Mathf.Clamp(player.position.y, yMin + camOrthsize, yMax - camOrthsize+100);
+        camX = Mathf.Clamp(player.position.x, xMin + cameraRatio, xMax - cameraRatio);
+        this.transform.position = new Vector3(camX, camY, this.transform.position.z);
+    }
+}
---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour{
+    public Camera cam;
+    public Transform subject;
+
+    Vector2 startPos;
+    float startZ;
+    float spriteWidth;
+
+    [SerializeField]
+    float speed = 0f;
+
+    float distanceFromSubject => transform.position.z - subject.position.z;
+    float clippingPlane => cam.transform.position.z + (distanceFromSubject > 0 ? cam.farClipPlane : cam.nearClipPlane);
+    float parallaxFactor => Mathf.Abs(distanceFromSubject) / clippingPlane;
+
+    // Start is called before the first frame update
+    void Start(){
+        startPos = transform.position;
+        startZ = transform.position.z;
+        spriteWidth = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void Update(){
+        Vector2 newPos = startPos + (Vector2)cam.transform.position * parallaxFactor;
+        transform.position = new Vector3(newPos.x+Time.time*-speed, newPos.y, startZ);
+
+        if (cam.transform.position.x > transform.position.x + spriteWidth)
+            startPos.x += spriteWidth;
+        else if (cam.transform.position.x < transform.position.x)
+            startPos.x -= spriteWidth;
+    }
+}
